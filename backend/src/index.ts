defb896f8e6cf9ed1ae41d1a12b5d4e3be1bcdd8
dev <thakurdev2672@gmail.com>
2024-11-27 02:06:47 +0530

    mongoDB and tailwind setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,14 @@
 import express,{Response,Request} from 'express';
 import cors from 'cors';
 import "dotenv/config";
+import mongoose from 'mongoose'
+
 
 
 
 const app=express()
 const PORT=8000;  
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
