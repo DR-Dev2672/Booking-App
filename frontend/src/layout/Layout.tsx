6c3ff27f3f7f469e03e739fbdd5c00f78e2b5820
dev <thakurdev2672@gmail.com>
2025-02-08 14:06:38 +0530

    layout added

---
+++ frontend/src/layout/Layout.tsx
@@ -0,0 +1,16 @@
+
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+
+function Layout() {
+  return (
+    <div className='flex flex-col min-h-screen'>
+        <Header/>
+        <Hero/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Layout
