a6c7cc09fd3b53248ff40a4200e4db80ce818dfc
dev <thakurdev2672@gmail.com>
2025-02-13 22:20:37 +0530

    api for cliet added

---
+++ frontend/src/api-client.ts
@@ -0,0 +1,24 @@
+import { RegisterFormData } from "./Pages/Register";
+
+const API_BASE_URL=import.meta.env.VITE_API_BASE_URL ||"";
+
+export const register=async(formData:RegisterFormData)=>{
+    const response=await fetch(`${API_BASE_URL}/api/users/register`,
+        {
+            method:"POST",
+            credentials:"include",
+            headers:{
+                "content-type":"application/json"
+            },
+            body:JSON.stringify(formData)
+
+        }
+    );
+    const responseBody=await response.json();
+
+    if(!response.ok){
+        throw new Error(responseBody.message);
+    }
+
+
+}
