c80a3b5311b1d2a573eea0237128818fc2979ddb
dev <thakurdev2672@gmail.com>
2025-02-28 23:44:44 +0530

    res.sendStatus and res.status is so so so.... different

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -5,24 +5,49 @@ const API_BASE_URL=import.meta.env.VITE_API_BASE_URL ||"";
 
 
 
+
+
+
+
+
 export const register = async (formData: RegisterFormData) => {
     const response = await fetch(`${API_BASE_URL}/api/users/register`, {
       method: "POST",
       credentials: "include",
+      
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(formData),
     });
-  
+    // console.log(response);
     const responseBody = await response.json();
+    
   
     if (!response.ok) {
       throw new Error(responseBody.message);
     }
   };
 
-export const signIn=async(formData:SignInFormData)=>{
+
+
+
+export const validateToken = async () => {
+    const response = await fetch(`${API_BASE_URL}/api/auth/validate-token`, {
+      credentials: "include",
+      
+      
+    });
+    // console.log("response.json")
+    if (!response.ok) {
+      throw new Error("Token invalid");
+    }
+  
+    return response.json();
+    
+  };
+
+  export const signIn=async(formData:SignInFormData)=>{
     const response =await fetch(`${API_BASE_URL}/api/auth/login`,{
         method:"POST",
         credentials:"include",
@@ -37,21 +62,6 @@ export const signIn=async(formData:SignInFormData)=>{
     }
     return body;
 }
-
-
-export const validateToken = async () => {
-    const response = await fetch(`${API_BASE_URL}/api/auth/validate-token`, {
-      credentials: "include",
-      
-    });
-  
-    if (!response.ok) {
-      throw new Error("Token invalid");
-    }
-  
-    return response.json();
-    console.log(response.json)
-  };
 
 export const signOut=async()=>{
     const response=await fetch(`${API_BASE_URL}/api/auth/logout`,{
