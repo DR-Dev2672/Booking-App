9e06cb967e2a2c5295fd595b42f108078c3cd1ef
dev <thakurdev2672@gmail.com>
2025-04-25 19:02:47 +0530

    search bar component in layout

--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -3,6 +3,7 @@
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import SearchBar from '../components/SearchBar'
 
 interface Props{
   children: React.ReactNode
@@ -13,6 +14,9 @@ function Layout({children}:Props) {
     <div className='flex flex-col min-h-screen'>
         <Header/>
         <Hero/>
+        <div className="container mx-auto">
+        <SearchBar />
+      </div>
         <div className='containe mx-auto flex-1 py-10'>{children}</div>
         <Footer/>
     </div>
