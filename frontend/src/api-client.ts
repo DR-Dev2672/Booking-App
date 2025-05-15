9bc88c61a7fe1429d6bb5b05841a4e5e1cacda27
dev <thakurdev2672@gmail.com>
2025-05-15 15:32:20 +0530

    createRoomBooking and createPaymentIntent added  for api call

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,6 +1,7 @@
 import { RegisterFormData } from "./Pages/Register";
 import { SignInFormData } from "./Pages/SignIn";
 import {HotelType,HotelSearchResponse, UserType} from "../../backend/src/shared/types";
+import { BookingFormData } from "./forms/BookingForm/BookingForm";
 
 const API_BASE_URL=import.meta.env.VITE_API_BASE_URL ||"";
 
@@ -191,6 +192,36 @@ export const fetchHotelById = async (hotelId: string): Promise<HotelType> => {
   const response = await fetch(`${API_BASE_URL}/api/hotels/${hotelId}`);
   if (!response.ok) {
     throw new Error("Error fetching Hotels");
+  }
+
+  return response.json();
+};
+
+export const createRoomBooking = async (formData: BookingFormData) => {
+  const response = await fetch(
+    `${API_BASE_URL}/api/hotels/${formData.hotelId}/bookings`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(formData),
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error("Error booking room");
+  }
+};
+
+export const fetchMyBookings = async (): Promise<HotelType[]> => {
+  const response = await fetch(`${API_BASE_URL}/api/my-bookings`, {
+    credentials: "include",
+  });
+
+  if (!response.ok) {
+    throw new Error("Unable to fetch bookings");
   }
 
   return response.json();
